fdcfd0036b0b1ac4df8752baded6a24fee6f280a
Roman <pixma6600@ukr.net>
2025-07-04 18:24:23 +0300

    last-888

--- part_4/task_1.py
+++ part_4/task_1.py
@@ -21,3 +21,12 @@ def get_days_from_today(date_str: str) -> int:
     delta = today - input_date
 
     return delta.days
+
+
+if __name__ == "__main__":
+    date_input = input("Введіть дату у форматі РРРР-ММ-ДД: ")
+    try:
+        days = get_days_from_today(date_input)
+        print(f"Кількість днів від {date_input} до сьогодні: {days}")
+    except ValueError as e:
+        print(e)
